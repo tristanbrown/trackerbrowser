f61ceda9503d4eaf6c4df34145b33838c0200135
tristanbrown <brown.tristan.r@gmail.com>
2023-06-08 00:31:06 +0000

    Use context to ensure db conn closes

--- src/trackerbrowser/tables.py
+++ src/trackerbrowser/tables.py
@@ -1,24 +1,31 @@
 """Get the different relevant tables from Track & Graph"""
 
 import pandas as pd
+from .connection import connect
 
-def list_tables(conn):
+def read_sql(path, query):
+    """"""
+    with connect(path) as conn:
+        result = pd.read_sql_query(query, conn)
+    return result
+
+def list_tables(path):
     """"""
     query = """SELECT name FROM sqlite_schema
         WHERE type='table' ORDER BY name;"""
-    return pd.read_sql_query(query, conn)
+    return read_sql(path, query)
 
-def get_groups(conn):
+def get_groups(path):
     """"""
     query = """SELECT * FROM groups_table"""
-    return pd.read_sql_query(query, conn)
+    return read_sql(path, query)
 
-def get_trackers(conn):
+def get_trackers(path):
     """"""
     query = """SELECT * FROM features_table"""
-    return pd.read_sql_query(query, conn)
+    return read_sql(path, query)
 
-def get_datapoints(conn):
+def get_datapoints(path):
     """"""
     query = """SELECT * FROM data_points_table"""
-    return pd.read_sql_query(query, conn)
+    return read_sql(path, query)
